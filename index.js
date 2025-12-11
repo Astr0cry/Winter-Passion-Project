2fab21bd14c6dab8b71b7f57e17f0bc26c122e9d
Maeson <maesoneser1219@gmail.com>
2025-12-11 10:37:16 -0500

    box bounces off of the screen

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const FPS = 30;
+const FPS = 60;
 const width = window.innerWidth;
 const height = window.innerHeight;
 class Vector2D{
@@ -13,27 +13,30 @@ class Vector2D{
     }
 
     divide(scalar){
-        this.x/scalar;
-        this.y/scalar;
+        this.x/=scalar;
+        this.y/=scalar;
+    }
+
+    multiply(scalar){
+        this.x*=scalar;
+        this.y*=scalar;
     }
 }
 
-GRAVITY = new Vector2D(0,-4.9);
+GRAVITY = new Vector2D(0,-120);
 GRAVITY.divide(FPS);
-
 class Object{
     constructor(name,mass,element){
         const s = getComputedStyle(element);
         this.name = name;
         this.mass = mass;
+        this.width = Number(s.getPropertyValue("width").replace("px",""));
+        this.height = Number(s.getPropertyValue("height").replace("px",""));
         this.element = element;
         this.velocity = new Vector2D(0,0);
-        console.log(this.velocity)
         this.acc = new Vector2D(0,0)
         this.acc.add(GRAVITY);
-        console.log(this.acc);
         this.pos = new Vector2D(Number(s.getPropertyValue("left").replace("px","")),Number(s.getPropertyValue("bottom").replace("px","")));
-        console.log(this.pos);
     }
 
     update(){
@@ -44,17 +47,18 @@ class Object{
     }
 
     handleEdges(width,height){
-        if (this.pos.x < 0 || this.pos.x < width){
-            this.acc.x = -this.acc.x;
+        if (this.pos.x <= 0 || this.pos.x+this.width > width){
+            this.velocity.x = -this.velocity.x;
         }
-        if (this.pos.y < 0 || this.pos.y < height){
-            this.acc.y = -this.acc.y;
+        if (this.pos.y <= 0 || this.pos.y+100> height){
+            this.velocity.y = -this.velocity.y;
         }
     }
 }   
 const square = new Object("square",10,document.getElementsByTagName("div")[0]);
 
 function frame(){
+    square.handleEdges(width,height);
     square.update();
 }
 
